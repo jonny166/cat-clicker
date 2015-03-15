aaf796d8fb54a1b895b5b0965adc23ea53b5e5b7
jonny166 <kathy.church@gmail.com>
2015-03-15 12:01:32 -0400

    Change to display a single cat selected from a list

--- js/script.js
+++ js/script.js
@@ -1,52 +1,62 @@
-var cats = []; //cats will be added to this
-var counterElements = [];
+var cats = {}; //cats will be added to this
 
 var catHTML = '<span class="cat-container">\
-    <img class="cat-image" id="cat%catNum%" src="%img%" alt="cat image" />\
+    <img class="cat-image" id="cat-image" src="%img%" alt="cat image" />\
     </div>\
     <div class="counter-container">\
-    <h3 class="counter" id="counter-label">You have clicked me this many times:&nbsp;</h3>\
-    <h2 class="counter" id="counter%catNum%" class="counter">0</h2>\
+    <h3 class="counter" id="counter-label">You have clicked %catName% this many times:&nbsp;</h3>\
+    <h2 class="counter" id="counter" class="counter">0</h2>\
     </span>';
+
 
 
 // Generates the cats when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-    var numCats = 4;
-    var cols = 3;
-    var s = 500;
+    // Create all the cats
+    cats["Spooky"] = new Cat("Spooky", "images/cat1.jpg");
+    cats["Ellie"] = new Cat("Ellie", "images/cat2.jpg");
+    cats["Andre"] = new Cat("Andre", "images/cat3.jpg");
+    cats["Katie"] = new Cat("Katie", "images/cat4.jpg");
+    cats["Fred"] = new Cat("Fred", "images/cat5.jpg");
 
-    for (var i = 0; i < numCats; i++) {
-        var cat = new Cat(i);
-        cats.push(cat);
-        var formattedHTML = catHTML.replace("%img%", cat.img).replace(/%catNum%/g, i);
-        $("#cats-container").append(formattedHTML);
+    //default to the first cat
+    var formattedHTML = catHTML.replace("%img%", cats["Spooky"].img).replace(/%catName%/g, cats["Spooky"].catID);
+    $("#cats-container").append(formattedHTML);
+
+    $.each(cats, function(catName, cat) {
         cat.setClick();
-        $("#cat" + i).basicLeft = (i % cols) * s;
-        //$("#cat" + i).style.top = (Math.floor(i / cols) * s) + 'px';
-    }
-    counterElements = document.querySelectorAll(".counter-container");
+    });
+
 });
 
 
 
-var Cat = function(id) {
+var Cat = function(catID, catIMG) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    console.log("creating cat " + id);
-    this.catID = id;
-    this.img = "http://placekitten.com/g/500/200";
+    console.log("creating cat " + catID);
+    this.catID = catID;
+    this.img = catIMG;
     this.counter = 0;    
+
+    $('#cat-list').append(
+        $('<li>').append(
+            $('<span>').attr('id', 'cat'+catID).append(catID)
+        ));
 };
 
 Cat.prototype.setClick = function() {
     var catID = this.catID;
     $('#cat'+this.catID).click(function(e) {
         //the element has been clicked... do stuff here
-        var $counterElem = $('#counter'+catID);
+        var $counterElem = $('#counter');
+        var $catElem = $('#cat-image');
+        var $counterLabelElem = $('#counter-label');
+        $catElem.attr('src', cats[catID].img);
+        $counterLabelElem.text("You have clicked " + catID + " this many times: ");
         console.log($counterElem);
         cats[catID].counter++;
         $counterElem.text(cats[catID].counter);
